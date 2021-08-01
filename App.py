cc43d59a9a5637b9901211b81e1fa42d740a6ecc
abreham <aynuyeabresh@gmail.com>
2021-08-01 05:51:38 -0300

    Working on Dashbard

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.set_page_config(page_title="Pharmacutical Sales predictio", layout="wide")
+st.set_page_config(pagetitle="Pharmacutical Sales predictio", layout="wide")
 
 
 
@@ -22,31 +22,3 @@ option = st.sidebar.selectbox('select result',('Raw Data',
 
 
 #st.title(option)
-
-||||||| 03dde00
-=======
-import streamlit as st
-import pandas as pd 
-import plotly.express as px
-import plotly.graph_objects as go
-
-st.set_page_config(page_title="Pharmacutical Sales predictio", layout="wide")
-
-
-
-#def loadData():
-#    print("loading started ")
-#    dataframe = pd.read_csv("./features/my_features.csv")
-#    print("loading complleted")
-#    return dataframe
-
-#dataframe =  loadData()
-
-st.sidebar.title("Pharmacutical-sales-Prediction")
-option = st.sidebar.selectbox('select result',('Raw Data',
-'Plots',' prediction','insight'))
-
-
-#st.title(option)
-
->>>>>>> f74efb13af341ac47438f7a8c82c6d794ff15101
