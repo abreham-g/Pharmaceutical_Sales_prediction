4ea77b7a063e8444a17af6335248ef6ad7901e67
abreham <aynuyeabresh@gmail.com>
2021-08-01 05:49:41 -0300

    Working on dashboard

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
@@ -8,13 +7,3 @@ enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
 ||||||| 03dde00
-=======
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
->>>>>>> f74efb13af341ac47438f7a8c82c6d794ff15101
